cd0eefd0e0d364e2edd009cdd1b7f426d99485bb
Ankit Kumar <ak84@iitbbs.ac.in>
2022-10-19 03:37:36 +0530

    updated.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="mt-24">
+    <p className="dark:text-gray-200 text-gray-700 text-center m-20">
+    COPYRIGHT © 2022. ALL RIGHTS RESERVED. DESIGNED BY ANKIT KUMAR
+    </p>
+  </div>
+);
+
+export default Footer;
